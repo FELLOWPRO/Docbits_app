71fd9d3e098335deadda928852f32adb28113f21
dajor <daniel.jordan@fellow-consulting.de>
2026-05-12 03:59:49 +0000

    [ci skip] Deploy SPA+PWA for sandbox (version 10.9.3)

---
+++ assets/enum.C4E_7yly.js
@@ -0,0 +1,2 @@
+(function(){try{var e=typeof window<`u`?window:typeof global<`u`?global:typeof globalThis<`u`?globalThis:typeof self<`u`?self:{};e.SENTRY_RELEASE={id:`9b8d2f35ecc755803885acc72c9aee9844275ee7`};var t=new e.Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]=`a1fec2e9-6a5c-4e6f-a70b-a5b477c8bf56`,e._sentryDebugIdIdentifier=`sentry-dbid-a1fec2e9-6a5c-4e6f-a70b-a5b477c8bf56`)}catch{}})();var e=[`low`,`medium`,`high`],t=[`open`,`in_progress`,`done`,`rejected`];export{t as n,e as t};
+//# sourceMappingURL=enum.C4E_7yly.js.map
